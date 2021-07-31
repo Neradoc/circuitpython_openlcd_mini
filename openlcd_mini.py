fbf99319f02b934a2274155267b5f27d5dcee74f
Neradoc <Neradoc@users.noreply.github.com>
2021-07-31 19:40:10 +0200

    use properties

--- openlcd_mini.py
+++ openlcd_mini.py
@@ -8,6 +8,8 @@ class OpenLCD:
     def __init__(self, i2c, address):
         self.address = address
         self.i2c = i2c
+        self._color = (255,255,255)
+        self._contrast = 0
 
     def clear(self):
         while not self.i2c.try_lock():
@@ -22,21 +24,40 @@ class OpenLCD:
         self.i2c.unlock()
 
     def write(self, value):
-        # TODO: replace | with ||
-        self.send(b"{}".format(value))
+        out = b"{}".format(value.replace("|","||"))
+        self.send(out)
 
-    # lcd.send(b"|+\x00\xff\xff")
+    def print(self, *values, end="\r", sep=" "):
+        self.write(sep.join([str(x) for x in values])+end)
+
+    @property
+    def backlight(self):
+        return self._color
+
+    @backlight.setter
     def backlight(self, color):
         command = b"|+"
         if (type(color) == tuple or type(color) == list) and len(color) == 3:
             command += bytes(color)
+            self._color = tuple(color)
         elif type(color) == int:
             cb = (color // 0x10000 & 0xFF, color // 0x100 & 0xFF, color & 0xFF)
             command += bytes(cb)
+            self._color = cb
+        else:
+            raise ValueError("color format: (r,g,b) or 0xRRGGBB")
         self.send(command)
 
+    @property
+    def contrast(self):
+        return self._contrast
+
+    @contrast.setter
     def contrast(self, value):
-        command = b"|" + bytes((24, value & 0xFF))
+        if int(value) < 0 or int(value) > 0xFF:
+            raise ValueError("contrast is a value between 0 and 255")
+        command = b"|" + bytes((24, int(value)))
+        self._contrast = value
         self.send(command)
 
     def move(self, x, y):
